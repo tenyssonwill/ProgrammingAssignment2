8bc55c7a13e6cf46228b6ff6d874702dc8ad7424
Tenysson <tenyssonwill@gmail.com>
2020-08-02 17:30:01 -0300

    Firt Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## File have 2 functions using objects
 
-## Write a short comment describing this function
+## This function create a matrix object that has 4 methods and a constructor: 
+## set, get, set_InvMatrix and get_InvMatrix. The input object must be of 
+## class matrix(). The once the inverse matrix is calculated it's cache in the object. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_matrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inv_matrix <<- NULL
+  }
+  get <- function() x
+  set_InvMatrix <- function(inv) inv_matrix <<- inv
+  get_InvMatrix <- function() inv_matrix
+  list(set = set, get = get,
+       set_InvMatrix = set_InvMatrix,
+       get_InvMatrix = get_InvMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function calculate the inverse matrix of the object create by the 
+## function makeCacheMatrix. If the object already has a inverse matrix 
+## skip the calculation and just recover the inverse matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$get_InvMatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$set_InvMatrix(m)
+  m
 }
